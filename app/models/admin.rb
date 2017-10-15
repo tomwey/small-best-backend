a2edcb19d3054bfe760de035ea29f8c3e96034dc
tomwey <tomwey@163.com>
2017-10-15 11:40:09 +0800

    change some func

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,8 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         # :recoverable, 
+         :rememberable, :trackable, :validatable
   
   # 超级管理员
   def super_admin?
